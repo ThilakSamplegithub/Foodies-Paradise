98341b71efaf9e9255a5576a67f08cf70ba19b7b
Ankit Kumar <ankitkrai4n0522@gmail.com>
2023-06-18 14:33:57 +0530

    Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
-import { MainRoutes } from './Pages/MainRoutes';
-
+import "./App.css";
+import { MainRoutes } from "./Pages/MainRoutes";
 function App() {
   return (
     <div className="App">
-      <MainRoutes /> 
+      <MainRoutes />
     </div>
   );
 }
